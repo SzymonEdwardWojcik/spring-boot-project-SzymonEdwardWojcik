47627d4d2c3475287b6176c60b3a19be4cce6586
wutismyname <plebejoosh@gmail.com>
2018-01-16 13:05:59 +0100

    Add teatype service with basic methods

--- src/main/java/com/codecool/boot/teatype/TeaTypeService.java
+++ src/main/java/com/codecool/boot/teatype/TeaTypeService.java
@@ -1,4 +1,23 @@
 package com.codecool.boot.teatype;
 
 public class TeaTypeService {
+
+    private TeaTypeRepository repository;
+
+    public TeaTypeService(TeaTypeRepository repository) {
+        this.repository = repository;
+    }
+
+    public Iterable<TeaType> findAll() {
+        return this.repository.findAll();
+    }
+
+    public TeaType findOne(Integer id) {
+        return this.repository.findOne(id);
+    }
+
+    public TeaType save(TeaType teaType) {
+        this.repository.save(teaType);
+        return teaType;
+    }
 }
