2f18b5d214650128c957bf95319e750435fa0280
wutismyname <plebejoosh@gmail.com>
2018-01-16 13:04:35 +0100

    Add tea model

--- src/main/java/com/codecool/boot/tea/Tea.java
+++ src/main/java/com/codecool/boot/tea/Tea.java
@@ -1,4 +1,19 @@
 package com.codecool.boot.tea;
 
+import com.codecool.boot.teatype.TeaType;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+@Entity
 public class Tea {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Integer price;
+    @OneToOne
+    @Cascade(org.hibernate.annotations.CascadeType.PERSIST)
+    private TeaType teaType;
 }
