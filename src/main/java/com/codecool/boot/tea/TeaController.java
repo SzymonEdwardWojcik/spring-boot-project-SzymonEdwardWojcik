08fc5fcf0898d076a1233e5aba12a56a2b6afc54
wutismyname <plebejoosh@gmail.com>
2018-01-16 13:04:59 +0100

    Add tea controller file with basic methods

--- src/main/java/com/codecool/boot/tea/TeaController.java
+++ src/main/java/com/codecool/boot/tea/TeaController.java
@@ -1,4 +1,25 @@
 package com.codecool.boot.tea;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/teas")
 public class TeaController {
+
+    private TeaRepository repository;
+
+    public TeaController(TeaRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(path = "")
+    public Iterable<Tea> index() {
+        return this.repository.findAll();
+    }
+
+    @PostMapping(path = "")
+    public Tea create(@RequestBody Tea tea) {
+        this.repository.save(tea);
+        return tea;
+    }
 }
