8b7bc598de03e2457a6ae603c886f44b70e9e4e4
wutismyname <plebejoosh@gmail.com>
2018-01-16 15:02:51 +0100

    Add getters and setters

--- src/main/java/com/codecool/boot/tea/Tea.java
+++ src/main/java/com/codecool/boot/tea/Tea.java
@@ -16,4 +16,42 @@ public class Tea {
     @OneToOne
     @Cascade(org.hibernate.annotations.CascadeType.PERSIST)
     private TeaType teaType;
+
+    public Tea(String name, Integer price, TeaType teaType) {
+        this.name = name;
+        this.price = price;
+        this.teaType = teaType;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public TeaType getTeaType() {
+        return teaType;
+    }
+
+    public void setTeaType(TeaType teaType) {
+        this.teaType = teaType;
+    }
 }
