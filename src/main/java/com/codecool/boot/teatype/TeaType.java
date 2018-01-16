de88298e0427110f585ca0c5fa6c7cc72c667409
wutismyname <plebejoosh@gmail.com>
2018-01-16 15:03:00 +0100

    Add getters and setters

--- src/main/java/com/codecool/boot/teatype/TeaType.java
+++ src/main/java/com/codecool/boot/teatype/TeaType.java
@@ -14,9 +14,35 @@ public class TeaType {
     private String type;
     private String origin;
 
+    public TeaType() {
+    }
+
     public TeaType (String type, String origin) {
         this.type = type;
         this.origin = origin;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
 }
