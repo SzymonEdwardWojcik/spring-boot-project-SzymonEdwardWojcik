98e2651b08e90c3e33aa911f99a01a4f66384990
wutismyname <plebejoosh@gmail.com>
2018-01-16 15:02:39 +0100

    Fix wrong generic construction in service interface

--- src/main/java/com/codecool/boot/common/Service.java
+++ src/main/java/com/codecool/boot/common/Service.java
@@ -1,4 +1,10 @@
 package com.codecool.boot.common;
 
-public interface Service {
+public interface Service<T> {
+
+    Iterable<T> findAll();
+    T findOne(Integer id);
+    T save(T t);
+    void delete(T t);
+    void deleteById(Integer id);
 }
