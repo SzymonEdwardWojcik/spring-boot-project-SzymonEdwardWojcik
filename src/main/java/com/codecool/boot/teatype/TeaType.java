405d28e555ba7cd4772ef36f1f267dabd630af1b
wutismyname <plebejoosh@gmail.com>
2018-01-16 13:05:30 +0100

    Add teatype model

--- src/main/java/com/codecool/boot/teatype/TeaType.java
+++ src/main/java/com/codecool/boot/teatype/TeaType.java
@@ -1,4 +1,22 @@
 package com.codecool.boot.teatype;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class TeaType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String type;
+    private String origin;
+
+    public TeaType (String type, String origin) {
+        this.type = type;
+        this.origin = origin;
+    }
+
 }
