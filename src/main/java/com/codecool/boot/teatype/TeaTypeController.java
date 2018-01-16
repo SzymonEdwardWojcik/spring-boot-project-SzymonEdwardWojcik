8e1e33f6107b38fedb98b494bfa80a44266b4742
wutismyname <plebejoosh@gmail.com>
2018-01-16 13:05:44 +0100

    Add teatype controller with basic methods

--- src/main/java/com/codecool/boot/teatype/TeaTypeController.java
+++ src/main/java/com/codecool/boot/teatype/TeaTypeController.java
@@ -1,4 +1,30 @@
 package com.codecool.boot.teatype;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/types")
 public class TeaTypeController {
+
+    private TeaTypeService service;
+
+    public TeaTypeController(TeaTypeService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "")
+    public Iterable<TeaType> index() {
+        return this.service.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public TeaType show(@PathVariable Integer id) {
+        return this.service.findOne(id);
+    }
+
+    @PostMapping(path = "")
+    public TeaType create(@RequestBody TeaType teaType) {
+        this.service.save(teaType);
+        return teaType;
+    }
 }
