9a1246946a2549c7da12e4ed2f42cba2b8992b1c
wutismyname <plebejoosh@gmail.com>
2018-01-18 17:54:09 +0100

    Add error logging

--- src/main/java/com/codecool/boot/common/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/codecool/boot/common/RestResponseEntityExceptionHandler.java
@@ -2,30 +2,29 @@ package com.codecool.boot.common;
 
 import com.codecool.boot.common.exceptions.ApiError;
 import com.codecool.boot.common.exceptions.NoSuchIdException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+    LoggerInterface logger;
+
+    public RestResponseEntityExceptionHandler(LoggerInterface logger) {
+        this.logger = logger;
+    }
+
     @ExceptionHandler(NoSuchIdException.class)
     protected ResponseEntity<ApiError> handleNoSuchIdException() {
 
+        String message = "There is no record with such ID";
         ApiError apiError = new ApiError(HttpStatus.NOT_FOUND,
-                "There is no record with such ID");
+                message);
+
+        logger.logError(message);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
     }
 
