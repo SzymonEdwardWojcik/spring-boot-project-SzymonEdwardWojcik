91e63aea7798158675d1bba59f7c10b236fcdad3
wutismyname <plebejoosh@gmail.com>
2018-01-18 17:55:03 +0100

    Add logging of CRUD actions

--- src/main/java/com/codecool/boot/teatype/TeaTypeService.java
+++ src/main/java/com/codecool/boot/teatype/TeaTypeService.java
@@ -1,5 +1,6 @@
 package com.codecool.boot.teatype;
 
+import com.codecool.boot.common.LoggerInterface;
 import com.codecool.boot.common.Service;
 import com.codecool.boot.common.exceptions.NoSuchIdException;
 import com.codecool.boot.tea.Tea;
@@ -8,13 +9,16 @@ import com.codecool.boot.tea.Tea;
 public class TeaTypeService implements Service<TeaType> {
 
     private TeaTypeRepository repository;
+    private LoggerInterface logger;
 
-    public TeaTypeService(TeaTypeRepository repository) {
+    public TeaTypeService(TeaTypeRepository repository, LoggerInterface logger) {
         this.repository = repository;
+        this.logger = logger;
     }
 
     @Override
     public Iterable<TeaType> findAll() {
+        logger.logReadAll();
         return this.repository.findAll();
     }
 
@@ -25,20 +29,27 @@ public class TeaTypeService implements Service<TeaType> {
         if (teaType == null) {
             throw new NoSuchIdException();
         }
+        logger.logReadOne(teaType.toString());
         return teaType;
     }
 
     @Override
     public TeaType save(TeaType teaType) {
-        return this.repository.save(teaType);
+
+        this.repository.save(teaType);
+        logger.logCreate(teaType.toString());
+        return teaType;
     }
 
     @Override
     public TeaType put(TeaType teaType) {
-        return this.repository.save(teaType);
+        this.repository.save(teaType);
+        logger.logUpdate(teaType.toString());
+        return teaType;
     }
 
     @Override
+    @Deprecated
     public void deleteById(Integer id) {
         this.repository.delete(id);
     }
@@ -48,6 +59,7 @@ public class TeaTypeService implements Service<TeaType> {
         teaType.setArchived(true);
         archiveTeas(teaType);
         this.repository.save(teaType);
+        logger.logArchive(teaType.toString());
     }
 
     public void archiveTeas(TeaType teaType) {
