faa74c153975b51cabe7dbb39a604e6b6665dd61
wutismyname <plebejoosh@gmail.com>
2018-01-18 13:57:33 +0100

    Delete deprecated method

---
+++ src/main/java/com/codecool/boot/common/exceptions/ApiError.java
@@ -0,0 +1,40 @@
+package com.codecool.boot.common.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+
+    public ApiError(HttpStatus status, String message) {
+        super();
+        this.status = status;
+        this.message = message;
+    }
+
+    public ApiError(HttpStatus status, String message, String error) {
+        super();
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public static class NoSuchIdException extends Exception {
+    }
+}
