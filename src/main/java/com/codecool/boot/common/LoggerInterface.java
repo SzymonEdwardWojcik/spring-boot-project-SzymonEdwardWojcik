7b5b3d1ee9d8d281cfa5f0f4ec6a2ffd236abe2c
wutismyname <plebejoosh@gmail.com>
2018-01-18 17:53:43 +0100

    Add interface for loggers

--- src/main/java/com/codecool/boot/common/LoggerInterface.java
+++ src/main/java/com/codecool/boot/common/LoggerInterface.java
@@ -1,4 +1,11 @@
 package com.codecool.boot.common;
 
 public interface LoggerInterface {
+
+    void logError(String message);
+    void logCreate(String message);
+    void logReadOne(String message);
+    void logReadAll();
+    void logUpdate(String message);
+    void logArchive(String message);
 }
