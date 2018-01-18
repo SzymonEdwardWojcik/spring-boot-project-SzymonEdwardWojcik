c42e864feea6eb23dbcdf0b29be25e28ac9cff03
wutismyname <plebejoosh@gmail.com>
2018-01-18 17:53:57 +0100

    Add log4j logger implementation

--- src/main/java/com/codecool/boot/common/Log4jLogger.java
+++ src/main/java/com/codecool/boot/common/Log4jLogger.java
@@ -1,12 +1,40 @@
 package com.codecool.boot.common;
 
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
 
-public class LoggerService {
+@Component
+public class Log4jLogger implements LoggerInterface {
 
-    private final static Logger logger = Logger.getLogger(LoggerService.class.getName());
+    private final static Logger logger = Logger.getLogger(Log4jLogger.class.getName());
 
-    public void writeLog() {
+    @Override
+    public void logError(String message) {
+        logger.error(message);
+    }
 
+    @Override
+    public void logCreate(String json) {
+        logger.info("Created new record: " + json);
+    }
+
+    @Override
+    public void logReadOne(String json) {
+        logger.info("Read object: " + json);
+    }
+
+    @Override
+    public void logReadAll() {
+        logger.info("Read all records");
+    }
+
+    @Override
+    public void logUpdate(String json) {
+        logger.info("Updated record to: " + json);
+    }
+
+    @Override
+    public void logArchive(String json) {
+        logger.info("Archived record: " + json);
     }
 }
