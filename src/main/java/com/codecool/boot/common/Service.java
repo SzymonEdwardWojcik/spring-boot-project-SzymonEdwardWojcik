87376409fb5343e48e4aa63f0dbe4c4a5f352e8b
wutismyname <plebejoosh@gmail.com>
2018-01-18 15:50:26 +0100

    Add throwing no such id excptn to findOne() def

--- src/main/java/com/codecool/boot/common/Service.java
+++ src/main/java/com/codecool/boot/common/Service.java
@@ -1,9 +1,11 @@
 package com.codecool.boot.common;
 
+import com.codecool.boot.common.exceptions.NoSuchIdException;
+
 public interface Service<T> {
 
     Iterable<T> findAll();
-    T findOne(Integer id);
+    T findOne(Integer id) throws NoSuchIdException;
     T save(T t);
     T put(T t);
     void deleteById(Integer id);
