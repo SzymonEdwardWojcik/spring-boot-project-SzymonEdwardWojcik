8a6e2c66f6d76534849ef9bef2ace250696e6be7
wutismyname <plebejoosh@gmail.com>
2018-01-18 17:52:34 +0100

    Add log4j config

---
+++ src/main/java/com/codecool/boot/common/Log4jLogger.java
@@ -0,0 +1,12 @@
+package com.codecool.boot.common;
+
+import org.apache.log4j.Logger;
+
+public class LoggerService {
+
+    private final static Logger logger = Logger.getLogger(LoggerService.class.getName());
+
+    public void writeLog() {
+
+    }
+}
