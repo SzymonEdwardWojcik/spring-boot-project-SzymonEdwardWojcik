807d8991c1e5e09a8caccaac2a2ac5395dbc69b9
wutismyname <plebejoosh@gmail.com>
2018-01-19 10:38:26 +0100

    Add findAllByIsArchivedIsFalse()

--- src/main/java/com/codecool/boot/teatype/TeaTypeRepository.java
+++ src/main/java/com/codecool/boot/teatype/TeaTypeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TeaTypeRepository extends CrudRepository<TeaType, Integer>{
 
+    Iterable<TeaType> findAllByIsArchivedIsFalse();
 }
