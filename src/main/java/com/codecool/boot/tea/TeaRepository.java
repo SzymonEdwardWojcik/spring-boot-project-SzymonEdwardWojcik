ab480e60f5dd8701194ed982b758aad69f340d33
wutismyname <plebejoosh@gmail.com>
2018-01-19 10:37:22 +0100

    Add findAllByIsArchivedIsFalse()

--- src/main/java/com/codecool/boot/tea/TeaRepository.java
+++ src/main/java/com/codecool/boot/tea/TeaRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface TeaRepository extends CrudRepository<Tea, Integer> {
 
+    Iterable<Tea> findAllByIsArchivedIsFalse();
 }
